7e3d865bb48e09a094eb993d8adb6b8c8eb22cc7
SClarke929 <sthwaites929@gmail.com>
2018-12-12 21:48:41 -0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HELLO OBDULIA!");
+/* Trying to finish Friday's homework with as little difficulty as possible.
+ * Slowly, but surely we're making it.
+ */
+	}
+
+}
